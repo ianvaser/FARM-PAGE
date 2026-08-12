b956a957840dce41ab71b12162b7eb2be1260a13
Ian Vaserman <ivaserman@frba.utn.edu.ar>
2026-08-12 15:56:28 -0300

    login con google

--- src/components/InicioSesionForm.jsx
+++ src/components/InicioSesionForm.jsx
@@ -1,7 +1,10 @@
 import InicioSesion from '../pages/inicio-sesion';
 import '../stylesheets/inicio-sesion.css';
 import {useState} from 'react';
-
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import{auth} from '../data/firebase.js';
+import { FcGoogle } from "react-icons/fc";
+import{FaUser,FaLock,FaSignInAlt,FaUserPlus} from "react-icons/fa";
 export const InicioSesionForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -52,7 +55,30 @@ export const InicioSesionForm = () => {
 
     });
     }
-    const registro = () =>{
+
+const provider = new GoogleAuthProvider();
+const loginGoogle = async () => {
+    try {
+           const result = await signInWithPopup(auth, provider);
+           const user = result.user;
+           console.log(user);
+           const token = await user.getIdToken();
+           await fetch("http://localhost:3001/login/google", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    token
+                })
+            });
+        } 
+    catch (error) {
+         console.error(error);
+        }
+    };
+
+    const Registro = () =>{
         const data = {
             email: email,
             password:password
@@ -99,6 +125,8 @@ export const InicioSesionForm = () => {
     return(
         <form onSubmit={handleSubmit} className='inicio-sesion-form'>
             <label className='form-label' htmlFor="email">Email:</label>
+            <div className='input-group'>
+            <FaUser />
             <input 
                 className='form-input' 
                 type="email" 
@@ -108,8 +136,11 @@ export const InicioSesionForm = () => {
                 onChange={(e) => setEmail(e.target.value)} 
                 required 
             />
+            </div>
             <br />
             <label className='form-label' htmlFor="password">Contraseña:</label>
+            <div className='input-group'>
+            <FaLock />
             <input 
                 className='form-input' 
                 type="password" 
@@ -119,9 +150,11 @@ export const InicioSesionForm = () => {
                 onChange={(e) => setPassword(e.target.value)} 
                 required 
             />
+            </div>
             <br />
-            <button className='form-button' type="submit" onClick={e => InicioSesion()}>Iniciar sesión</button>
-            <button className='form-button' type="button" onClick={e => registro()}>Registrarse</button>
+            <button className='form-button' type="submit" onClick={e => InicioSesion()}><FaSignInAlt/>Iniciar sesión</button>
+            <button className='form-button' type="button" onClick={e => loginGoogle()}><FcGoogle size={30} /></button>    
+            <button className='form-button' type="button" onClick={e => Registro()}><FaUserPlus/>Registrarse</button>
         </form>
     );
 }
